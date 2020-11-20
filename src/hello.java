09da8c7e980001a2f4003c6656e1f749cb4e96ad
serdem21 <ayducity1@gmail.com>
2020-11-20 11:16:06 -0800

    Projrct is already in remote, commiting more changes

--- src/hello.java
+++ src/hello.java
@@ -1,4 +1,9 @@
 public class hello {
     // more stuff
     //mac user is so happu
+    // I have already publish on GitHub.com
+    //now i am ready to make commits
+    // but do i need to share again????
+    //Absolutely no
+    //IT'S TIME TO COMMMIT ANDJUST PUSH
 }
